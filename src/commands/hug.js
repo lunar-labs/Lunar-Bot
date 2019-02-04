534bddbb8120340488c8086e623831e7f3a1a612
Colin Moulds <colin.moulds1998@gmail.com>
2019-02-04 22:48:00 +0000

    Fixed Commands

--- src/commands/hug.js
+++ src/commands/hug.js
@@ -3,23 +3,22 @@ const util = require("util");
 
 module.exports.run = async(client, msg) => {
 
-  let hugged = msg.mentions.members.first();
-  const rando_imgs = [
-'https://media.giphy.com/media/lXiRKBj0SAA0EWvbG/giphy.gif',
-'https://media.giphy.com/media/13YrHUvPzUUmkM/giphy.gif',
-'https://media.giphy.com/media/BXrwTdoho6hkQ/giphy.gif',
-]
-if (!hugged) {
-  return msg.reply("you need to mention someone first to hug them");
+    let hugged = msg.mentions.members.first();
+    const rando_imgs = [
+        'https://media.giphy.com/media/lXiRKBj0SAA0EWvbG/giphy.gif',
+        'https://media.giphy.com/media/13YrHUvPzUUmkM/giphy.gif',
+        'https://media.giphy.com/media/BXrwTdoho6hkQ/giphy.gif',
+    ]
+    if (!hugged) {
+        return msg.reply("you need to mention someone first to hug them");
+    }
+
+    return msg.channel.send(`${message.author} gave ${hugged} a hug!`, {
+        file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
+    });
 }
 
-return msg.channel.send(`${message.author} gave ${hugged} a hug!`, {
-    file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
-});
-
-
-
 module.exports.help = {
-    name : "hug",
-    type: "User",
+    name : "eval",
+    type: "Owner",
 }
