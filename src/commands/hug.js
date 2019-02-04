51dddb224e041e1dc41dbf2b9a40eaac48546ea1
Paul <careypjr1993@gmail.com>
2019-02-03 20:04:36 -0600

    Adding hug command and updating folders

---
+++ src/commands/hug.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const util = require("util");
+
+module.exports.run = async(client, msg) => {
+
+  let hugged = msg.mentions.members.first();
+  const rando_imgs = [
+'https://media.giphy.com/media/lXiRKBj0SAA0EWvbG/giphy.gif',
+'https://media.giphy.com/media/13YrHUvPzUUmkM/giphy.gif',
+'https://media.giphy.com/media/BXrwTdoho6hkQ/giphy.gif',
+]
+if (!hugged) {
+  return msg.reply("you need to mention someone first to hug them");
+}
+
+return msg.channel.send(`${message.author} gave ${hugged} a hug!`, {
+    file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
+});
+
+
+
+module.exports.help = {
+    name : "hug",
+    type: "User",
+}
