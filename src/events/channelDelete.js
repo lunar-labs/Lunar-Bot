fe3a718f43cfac2a10634082c8636cc148eb4dd6
Paul <careypjr1993@gmail.com>
2019-02-05 20:11:41 -0600

    Adding Events

---
+++ src/events/channelDelete.js
@@ -0,0 +1,3 @@
+module.exports = async (channel) => {
+console.log(`channelDelete: ${channel}`);
+}
---
+++ src/events/guildCreate.js
@@ -0,0 +1,4 @@
+module.exports = async (guild, client) => {
+     console.log(`the client joined a guild`);
+
+}
