a79f48cb7946af7a441fd4a10736ef15ac61168e
Paul <careypjr1993@gmail.com>
2019-02-08 10:28:05 -0600

    update urls

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,5 +2,6 @@ module.exports = async client => {
   // Log that the bot is online.
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
   await client.website.load(client);
+  
   await client.user.setActivity(`${client.config.defaultSettings.prefix}help`, {type: "PLAYING"});
 };
