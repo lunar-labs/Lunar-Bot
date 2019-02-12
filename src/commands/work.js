30c4d37f35b8c226d04cc6b56dbca3ece9300768
Paul <careypjr1993@gmail.com>
2019-02-11 19:17:25 -0600

    removing

--- src/commands/work.js
+++ src/commands/work.js
@@ -3,14 +3,6 @@ const moment = require('moment');
 const eco = require('discord-economy');
 module.exports.run = async(client, message) => {
 message.delete();
-  var output = await eco.Work(message.author.id)
-  //50% chance to fail and earn nothing. You earn between 1-100 coins. And you get one out of 20 random jobs.
-  if (output.earned == 0) return message.reply('Aww, you did not do your job well so you earned nothing!')
-  message.channel.send(`${message.author.username}
-You worked as a \` ${output.job} \` and earned :money_with_wings: ${output.earned}
-You now own :money_with_wings: ${output.balance}`)
-
-
   var output = await eco.Work(message.author.id, {
     failurerate: 10,
     money: Math.floor(Math.random() * 500),
