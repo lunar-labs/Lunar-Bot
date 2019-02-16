14ce8554299a8f26b42f27d75bafd52562b7288a
Paul <careypjr1993@gmail.com>
2019-02-16 15:36:38 -0600

    Updating Help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,20 +1,40 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message) => {
+exports.run = (client, message, args, level) => {
+  // If no specific command is called, show all filtered commands.
+  if (!args[0]) {
+    // Load guild settings (for prefixes and eventually per-guild tweaks)
+    const settings = message.settings;
 
-    let embed = new Discord.RichEmbed()
-	.setThumbnail(client.user.avatarURL)
-	.setColor(0xffffff)
-    .setTitle("Welcome to Lunar Beta!")
-    .setDescription("Commands")
-    .addField("8ball", "8-Ball reaches into the future, to find the answers to your questions")
-    .addField("hug", "gives a user a hug")
-    .addField("kiss", "gives a user a kiss")
-    .addField("punch", "punches a user")
-    .setFooter(`Beta Lunar © 2019`)
-    .setTimestamp();
-     message.channel.send(`Hello! **${message.author.username}** !`, embed);
+    // Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
+    const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
 
+    // Here we have to get the command names only, and we use that array to get the longest name.
+    // This make the help commands "aligned" in the output.
+    const commandNames = myCommands.keyArray();
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+
+    let currentCategory = "";
+    let output = `= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n`;
+    const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
+    sorted.forEach( c => {
+      const cat = c.help.category.toProperCase();
+      if (currentCategory !== cat) {
+        output += `\n== ${cat} ==\n`;
+        currentCategory = cat;
+      }
+      output += `${settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
+    });
+    message.channel.send(output, {code:"asciidoc"});
+  } else {
+    // Show individual command's help.
+    let command = args[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      if (level < client.levelCache[command.conf.permLevel]) return;
+      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}`, {code:"asciidoc"});
+    }
+  }
 };
 
 exports.conf = {
